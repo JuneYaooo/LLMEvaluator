98bd61a52f9b9592bbbecf87a87c85e0af80b08e
maokangkun <maokangkun@pjlab.org.cn>
2024-05-07 11:37:59 +0800

    [+] evaluate qa

--- test2.py
+++ test2.py
@@ -1,8 +1,15 @@
 import os
 import dotenv
+import datetime
+from datasets import load_dataset
 from langchain_core.documents import Document
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from ragas.run_config import RunConfig
+from ragas import evaluate
+from ragas.metrics import (
+    answer_relevancy,
+    faithfulness,
+)
 
 from src.ragas.common.read_files import process_markdown, extract_pdf, read_docx, process_docx, process_and_deduplicate, read_txt
 from src.ragas.testset.generator import TestsetGenerator
@@ -55,34 +62,58 @@ def load_documents():
                 documents.append(document)
     return documents
 
+def generate_qa():
+    documents = load_documents()
+    k_context = KContextEvolution(context_num=2)
+    multi_context = MultiContextEvolution(context_num=2)
+    distributions = {multi_context: 1} #{simple: 0.5, reasoning: 0.25, multi_context: 0.25, counterfactual:0.7,error_correction}
 
-documents = load_documents()
-k_context = KContextEvolution(context_num=2)
-multi_context = MultiContextEvolution(context_num=2)
-distributions = {multi_context: 1} #{simple: 0.5, reasoning: 0.25, multi_context: 0.25, counterfactual:0.7,error_correction}
-
-generator_llm = ChatOpenAI(model="gpt-3.5-turbo-16k")
-critic_llm = ChatOpenAI(model="gpt-3.5-turbo-16k")
-embeddings = OpenAIEmbeddings()
-generator = TestsetGenerator.from_langchain(
-    generator_llm,
-    critic_llm,
-    embeddings,
-    chunk_size=int(os.getenv('CHUNK_MAX_LENGTH'))
-)
-testset = generator.generate_with_langchain_docs(
-    documents,
-    test_size=5,
-    raise_exceptions=False,
-    with_debugging_logs=False,
-    distributions=distributions,
-    run_config=RunConfig(max_workers=1)
-)
+    generator_llm = ChatOpenAI(model="gpt-3.5-turbo-16k")
+    critic_llm = ChatOpenAI(model="gpt-3.5-turbo-16k")
+    embeddings = OpenAIEmbeddings()
+    generator = TestsetGenerator.from_langchain(
+        generator_llm,
+        critic_llm,
+        embeddings,
+        chunk_size=int(os.getenv('CHUNK_MAX_LENGTH'))
+    )
+    testset = generator.generate_with_langchain_docs(
+        documents,
+        test_size=5,
+        raise_exceptions=False,
+        with_debugging_logs=False,
+        distributions=distributions,
+        run_config=RunConfig(max_workers=1)
+    )
 
 
-test_res_df = testset.to_pandas()
-import datetime
-current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-if not os.path.exists('output'):
-    os.makedirs('output')
-test_res_df.to_csv(f"output/test_data_{current_time}.csv", index=False, encoding='utf-8')
+    test_res_df = testset.to_pandas()
+    
+    current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    if not os.path.exists('output'):
+        os.makedirs('output')
+    test_res_df.to_csv(f"output/test_data_{current_time}.csv", index=False, encoding='utf-8')
+
+def evaluate_qa():
+    amnesty_qa = load_dataset("/mnt/workspace/maokangkun/RAG_Eval/amnesty_qa", "english_v2", trust_remote_code=True)
+    llm = ChatOpenAI(model="gpt-3.5-turbo-16k")
+    embeddings = OpenAIEmbeddings()
+
+    result = evaluate(
+        amnesty_qa["eval"],
+        metrics=[
+            faithfulness,
+            answer_relevancy
+        ],
+        llm=llm,
+        embeddings=embeddings,
+        run_config=RunConfig(max_workers=1)
+    )
+    df = result.to_pandas()
+    # print(df.head())
+
+    current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    df.to_csv(f"output/eval_{current_time}.csv", index=False, encoding='utf-8')
+
+# generate_qa()
+evaluate_qa()
