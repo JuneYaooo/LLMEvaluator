4971710ab30e6f9a5839598926e1ac8941f76f49
maokangkun <maokangkun@pjlab.org.cn>
2024-05-11 16:36:43 +0800

    [+] plot result

--- test2.py
+++ test2.py
@@ -6,11 +6,13 @@ import queue
 import dotenv
 import datasets
 import datetime
+import requests
 import pandas as pd
 from rich import pretty
 from rich import inspect
 from rich import print as rp
 from pathlib import Path
+from retrying import retry
 from langchain_core.documents import Document
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from ragas.run_config import RunConfig
@@ -114,7 +116,7 @@ def answer_qa(i=0):
     test_qa = pd.read_excel(test_qa_file, sheet_name=1)
     print(len(test_qa))
 
-    model_list = ['Meta-Llama-3-8B-Instruct', 'pulse_v12_7b_gpt4_hf']
+    model_list = ['Meta-Llama-3-8B-Instruct', 'pulse_v12_7b_gpt4_hf', 'Qwen1.5-7B-Chat']
     model_name = model_list[i]
     model_path = f'/mnt/workspace/maokangkun/Model_Eval/models/{model_name}'
     device = f'cuda:{i}'
@@ -172,7 +174,8 @@ def answer_qa(i=0):
 
 def evaluate_qa():
     # model_name = "gpt-3.5-turbo-16k"
-    model_name = "Qwen1.5-7B-Chat"
+    # model_name = "Qwen1.5-7B-Chat"
+    model_name = "Meta-Llama-3-8B-Instruct"
 
     answer_dir = Path('data/llm_answers')
     eval_dir = Path('data/llm_evals')
@@ -180,7 +183,7 @@ def evaluate_qa():
     tobe_eval = []
     for ans_file in answer_dir.glob(f'{qa_id}_*.json'):
         eval_file = eval_dir / (ans_file.stem.replace('_answer', '') + f'_by_{model_name}.csv')
-        if not eval_file.exists():
+        if not eval_file.exists() and model_name not in ans_file.name:
             tobe_eval.append([ans_file, eval_file])
 
     print(tobe_eval)
@@ -207,16 +210,124 @@ def evaluate_qa():
         # print(df.head())
         df.to_csv(out_file, index=False, encoding='utf-8')
 
-def check():
-    with open('data/llm_answers/q04_Meta-Llama-3-8B-Instruct_answer.json', 'r') as f:
-        data = json.load(f)
-    
-    for d in data:
-        m = d['metadata']
-        if m and type(m[0]) is str:
-            print(d['id'])
+@retry(stop_max_attempt_number=10, wait_random_min=200, wait_random_max=400)
+def get_pulse_reply(prompt, model='base_model'):
+    api_url = 'https://mchatgpt-internal.dev.6ccloud.com/v1/api/completion/generate_stream'
+    headers = {
+        'content-type': 'application/json',
+        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzZjM0ODE2YS0xYTFmLTQ2ZTMtYWM5ZS00N2IzYWI5ZWI4ZjkiLCJleHAiOjIwMDQxOTQ1ODcsInNjb3BlcyI6WyJ1c2VyIl19.aBHWyQOt-yF-mIkG0RTm1i2j-46ZDQ7y7kdEqSBI2v8'
+    }
+    json_data = {
+        'action': 'To user',
+        'parent_messages': [
+            {
+                'action': 'From user',
+                'content': prompt
+            }
+        ],
+        'gen_kwargs': {
+            'model': model,
+            'num_return_sequences': 1,
+            'max_new_tokens': 256,
+        },
+    }
+
+    try:
+        with requests.post(api_url, headers=headers, json=json_data, stream=True, timeout=60) as res:
+            for data in res.iter_lines(decode_unicode=True):
+                if data and data != 'data: [DONE]': ret = data
+            return json.loads(ret[6:])['messages'][0]['content']['parts'][0]
+    except:
+        return None
+
+def answer_qa_pulse():
+    test_qa_file = sorted(Path('data/llm_questions').glob('*.xlsx'))[-1]
+    test_qa = pd.read_excel(test_qa_file, sheet_name=1)
+    print(len(test_qa))
+
+    model_name = 'pulse_v12_7b_gpt4_hf'
+
+    data = []
+    for _, d in tqdm.tqdm(list(test_qa.iterrows())):
+        contexts = [c for c in eval(d.contexts) if c]
+        context = '\n'.join([f'[context {i+1}]. {c}' for i, c in enumerate(contexts)])
+        if context:
+            context = '\n'+context
+        else:
+            context = 'None'
+
+        inp = question_answer_prompt.format(
+            question=d.question,
+            context=context
+        ).prompt_str
+
+        retry = 0
+        while retry <= 5:
+            ret = get_pulse_reply(inp, '123bv11')
+            m = re.findall(r'```{"answer": "(.*)", "verdict": ".*"}```', ret)
+            if m:
+                ans = m[0]
+                break
+            else:
+                ans = ret
+                retry += 1
+
+        data.append({
+            'id': d.order_number,
+            'question': d.question,
+            'answer': ans,
+            'contexts': contexts,
+            'ground_truth': d.ground_truth,
+            'metadata': [i for i in eval(d.metadata) if i],
+            'evolution_type': d.evolution_type,
+        })
+
+    out_file = f'data/llm_answers/{test_qa_file.stem}_{model_name}_answer.json'
+    with open(out_file, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+def plot_result():
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    sns.set_theme(style="whitegrid")
+    eval_dir = Path('data/llm_evals')
+
+    qv = 'q04'
+    data = pd.DataFrame([], columns=['question_id', 'model', 'evolution_type', 'metric', 'value'])
+    for eval_file in eval_dir.glob(f'{qv}_*.csv'):
+        df = pd.read_csv(eval_file)
+        model = eval_file.stem.split('_by_')[0][len(qv)+1:]
+        for _,row in df.iterrows():
+            data.loc[len(data)] = [row.id, model, row.evolution_type, 'faithfulness', row.faithfulness]
+            data.loc[len(data)] = [row.id, model, row.evolution_type, 'answer_relevancy', row.answer_relevancy]
+            data.loc[len(data)] = [row.id, model, row.evolution_type, 'answer_similarity', row.answer_similarity]
+
+    g = sns.catplot(
+        data=data, kind="bar",
+        x="metric", y="value", hue="model",
+        palette="dark", alpha=.6, height=6
+    )
+    g.despine(left=True)
+    g.set_axis_labels("", "")
+
+    plt.savefig(eval_dir / f'{qv}_overall.png', dpi=300)
+
+    evo_type = list(set(data.evolution_type))
+    _, axes = plt.subplots(2, 5, figsize=(20, 8), sharex=True, sharey=True)
+    for i, ax in enumerate(axes.flat):
+        d = data[data.evolution_type==evo_type[i]]
+        sns.barplot(x="metric", y="value", data=d, hue="model", ax=ax)
+        ax.get_legend().remove()
+        ax.set_title(evo_type[i])
+        ax.set_xlabel('')
+        ax.set_ylabel('')
+        ax.tick_params(axis='x', labelrotation=-45)
+
+    plt.legend(loc="upper left", fontsize="8", bbox_to_anchor=(1.05, 1.05))
+    plt.savefig(eval_dir / f'{qv}_items.png', dpi=300)
 
 # generate_qa()
-# answer_qa()
-evaluate_qa()
-# check()
+# answer_qa(2)
+# evaluate_qa()
+# answer_qa_pulse()
+plot_result()
